b28d66a501e75c342926cefa28f6770d7f274062
cdomdev <dominguez5493.cd@gmail.com>
2025-04-29 21:00:12 -0500

    feat: Implement middleware for token-based authentication and access control

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("bearer-token")?.value;
+  const { pathname } = request.nextUrl;
+
+  // Permitir el acceso libre al login
+  if (pathname === "/login") return NextResponse.next();
+
+  if (!token) {
+    const loginUrl = new URL("/login", request.url);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  // Si hay token, continuar con la navegación normal
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ["/((?!_next|favicon.ico|.*\\..*).*)"],
+};
