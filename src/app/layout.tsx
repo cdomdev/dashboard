c3a7a288bc8e808f7928b009512a4ca7fe286172
cdomdev <dominguez5493.cd@gmail.com>
2025-04-29 21:00:20 -0500

    feat: Add Toast component to layout for notifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
+import { Toast } from "./dashboard/components/Toast";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -13,6 +14,7 @@ export default async function LayoutRoot({
   return (
     <html lang="es">
       <body>
+        <Toast />
         <main>{children}</main>
       </body>
     </html>
