cb78275c4763eecd7fbd7b80f02d0fff4694df03
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-05-10 16:43:38 +0000

    feact: add page and layout for secction users

--- src/app/dashboard/usuarios/page.tsx
+++ src/app/dashboard/usuarios/page.tsx
@@ -1,23 +1,8 @@
-import { Back } from "../../../components/ui/custom/buttons/Back";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Usuarios",
-  description: "Pagina de usuarios",
-};
-
-export default function HomeUsuario() {
+export default function AdminPage() {
   return (
-    <section className="w-full min-h-dvh p-7 flex flex-col gap-5 ">
-      <section className="w-full px-10 pt-14 flex justify-between gap-5">
-        <div>
-          <Back url="/dashboard" />
-          <h1 className="text-lg font-bold md:text-2xl">Usuarios</h1>
-          <span>0</span>
-        </div>
-      </section>
-      <section className="shadow-md w-full rounded-md p-4 bg-white">
-      </section>
-    </section>
+    <>
+      <h2>Listado de administradores</h2>
+      <span>Aqio el conteido o tabla de administradores</span>
+    </>
   );
 }
