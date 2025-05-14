5fdb11e22df87f0daf6ee7aad72f76fa8dd2c156
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-05-14 06:01:22 +0000

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   images: {
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com', 'encrypted-tbn0.gstatic.com',
+      'www.tarjetaalkosto.com.co',],
   },
-  allowedDevOrigins: ['https://studio.firebase.google.com'],
+  allowedDevOrigins: [
+    'https://3000-firebase-dashboard-1746882853146.cluster-hf4yr35cmnbd4vhbxvfvc6cp5q.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;
