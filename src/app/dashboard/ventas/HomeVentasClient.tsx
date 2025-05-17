57995954d1d7d0c8b8ba822e63287725ae6820bd
cdomdev <dominguez5493.cd@gmail.com>
2025-05-17 17:09:06 -0500

    feat: add UserOrders and TablerOrder components for order management and details

--- src/app/dashboard/ventas/HomeVentasClient.tsx
+++ src/app/dashboard/ventas/HomeVentasClient.tsx
@@ -1,16 +1,22 @@
-"use client"
+"use client";
 
 import { HeaderPagesSection } from "@/components/HeaderPagesSection";
-// import { useState } from "react";
+import { useState } from "react";
 import { SalesList } from "./components/List";
 
 export default function HomeVentas() {
-  // const [count, setCount] = useState<number>(0)
+  const [count, setCount] = useState<number>(0);
   return (
     <>
-      <HeaderPagesSection href="/dashboard/ofertas/crear" title="Ventas" url="/dashboard" viewCount={true} />
+      <HeaderPagesSection
+        href="/dashboard/ofertas/crear"
+        title="Ventas"
+        url="/dashboard"
+        viewCount={true}
+        catCount={count}
+      />
       <section className="mt-10">
-        <SalesList />
+        <SalesList setCount={setCount} />
       </section>
     </>
   );
