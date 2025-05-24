0f2469ed7fbebddf733c1f0773c209671d6cb015
cdomdev <dominguez5493.cd@gmail.com>
2025-05-23 20:26:04 -0500

    feat: implement authentication API with login and token retrieval

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,45 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+const HOST = process.env.NEXT_PUBLIC_HOST_API;
+
+export async function POST(req: Request) {
+  const data = await req.json();
+
+  const response = await fetch(`${HOST}/api/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    return NextResponse.json({ result }, { status: response.status });
+  }
+
+  const { accessToken, refreshToken } = result;
+
+  const cookiesSession = await cookies();
+
+  cookiesSession.set("bearer-token", accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+  });
+
+  cookiesSession.set("bearer-token-rfsh", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    path: "/",
+  });
+
+return NextResponse.json({ response: response.status, result });
+
+}
---
+++ src/lib/getToken.ts
@@ -0,0 +1,16 @@
+
+export async function getToken() {
+  try {
+    const response = await fetch("/api/getToken", {
+      credentials: "include",
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data.token;
+    }
+    return null;
+  } catch (error) {
+    console.error("Error feching token", error);
+    return null;
+  }
+}
