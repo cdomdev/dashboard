85286afb9434570b89e6c1c77132feb3ef9382e6
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-05-26 04:49:26 +0000

    feat: add props for icons oferts

--- src/components/icons/Ofertas.tsx
+++ src/components/icons/Ofertas.tsx
@@ -1,6 +1,7 @@
-export const Ofertas = () => {
+export const Ofertas = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
