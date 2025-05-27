8e8e164262fb22bad27d824bd841144e079e914b
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-05-27 07:37:56 +0000

    feat: add header for tables elements

--- src/utils/headListForTables.ts
+++ src/utils/headListForTables.ts
@@ -1,5 +1,7 @@
+// datos para la cabecera de la tabla de productos
+
 export const itemsHeadTableProducts = [
-  "ID",
+  "",
   "Nombre del producto",
   "Marca",
   "Cantidad",
@@ -7,6 +9,64 @@ export const itemsHeadTableProducts = [
   "Precio",
   "Imagen",
   "Referencia",
+  "",
+  "",
+];
+
+
+// datos para la cabecera de la tabla de ordenes
+
+export const itemsHeadTableOrdes = [
+  "",
+  "Usuario",
+  "Total pagado",
+  "Método de pago",
+  "Costo de envío",
+  "Estado de Mercado Pago",
+  "Estado del pedido",
+  "Detalles",
+  "",
+];
+
+// datos para la cabecera de la tabla de ventas
+
+export const itemsHeadTableVentas = [
+  "",
+  "Usuario",
+  "Ciudad",
+  "Departamento",
+  "Teléfono",
+  "Detalles",
+];
+
+// datos para la cabecera de la tabla de usuarios
+
+export const itemsHeadTableUser = [
+  "",
+  "Nombre",
+  "Email",
+  "Estado",
+  "Rol",
+  "Avatar",
+  "Telefono",
+  "Ciudad",
+  "",
+  "",
+  "",
+];
+
+// datos para la cabecera de la tabla de categorias y subcategorias
+
+export const itemsHeadCatAndSub = ["", "Nombre de la categoria", "", ""];
+
+// datos para la cabecera de la tabla de ofertas
+
+export const itemsHeadTableOferts = [
+  "",
+  "Titulo de la oferta",
+  "Fecha de inicio",
+  "Fecha finalizacion",
+  "Imagen",
   "",
   "",
 ];
